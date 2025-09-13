c371d1af929d6b576f0aba15f74ffc9b9783c7d6
Ankit <ankit0525252@gmail.com>
2025-09-14 01:33:35 +0530

    initialised e-com repo

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import app from './app.js'
+import dotenv from 'dotenv';
+dotenv.config({path: 'backend/config/config.env'});
+
+app.get("/", (req, res)=>{
+  res.json({
+    message : 'working',
+  })
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log(`Server is running on PORT ${process.env.PORT}`);
+})
