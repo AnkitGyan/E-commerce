214f3033f312a35f7ee4fb27c4a84eb3efcbc31a
Ankit Sharma <ankit0525252@gmail.com>
2026-02-24 00:56:36 +0530

    created store using react-redux and react/toolkit

--- frontend/e-com-frontend/src/main.jsx
+++ frontend/e-com-frontend/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import {Provider} from 'react-redux';
 import './index.css'
 import App from './App.jsx'
+import  { store } from './app/store.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
