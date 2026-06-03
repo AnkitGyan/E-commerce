c390f8b28c97f90c4aad45c36935fb6788ddfdf0
Ankit Sharma <ankit0525252@gmail.com>
2026-06-03 11:35:38 +0530

    payment verification

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -22,3 +22,25 @@ export const sendAPIKey = wrapAsync(async (req, res) => {
     key: process.env.RAZORPAY_API_KEY,
   });
 });
+
+//Payment Verification
+
+export const paymentVerification = wrapAsync(async (req, res)=>{
+  const {razorpay_payment_id, razorpay_order_id, razorpay_signature} = req.body;
+
+  const sign = razorpay_order_id + "|" + razorpay_payment_id;
+  const expectedSign = crypto.createHmac('sha256', process.env.RAZORPAY_API_SECRET).update(sign.toString()).digest('hex');
+
+  if(expectedSign === razorpay_signature){
+    res.status(200).json({
+      success : true,
+      message : 'Payment Verified Successfully',
+      reference : razorpay_payment_id
+    })
+  }else{  
+    return res.status(200).json({
+      success: false,
+      message: "Payment verification failed",
+    });
+  }
+})
